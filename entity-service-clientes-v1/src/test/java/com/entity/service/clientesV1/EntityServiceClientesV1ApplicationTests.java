3ed186f3823e8c17d0274669510271944319e39d
moises1540 <braulio.moises.mtz.mdz@gmail.com>
2022-07-22 23:45:58 -0500

    entity-service-clientes-v1

---
+++ entity-service-clientes-v1/src/test/java/com/entity/service/clientesV1/EntityServiceClientesV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entity.service.clientesV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityServiceClientesV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
